67dbfd80dacb516b5f2e93d978906feee9336d2f
刘云龙 <liuyl@kaolafm.com>
2016-10-14 13:59:00 +0800

    1.一头圆一头方

--- app/src/main/java/com/novelot/demo/drawable/ScrollingActivity.java
+++ app/src/main/java/com/novelot/demo/drawable/ScrollingActivity.java
@@ -14,31 +14,24 @@ import android.widget.ImageView;
 
 public class ScrollingActivity extends AppCompatActivity {
 
-    private ImageView ivClip;
-    public int mDrawableLevel = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        setContentView(R.layout.content_scrolling);
 
         //
+        clip();
+    }
+
+    //Clip Demo
+    private ImageView ivClip;
+    public int mDrawableLevel = 0;
+
+    private void clip() {
         Button btnClip = (Button) findViewById(R.id.btnClip);
         ivClip = (ImageView) findViewById(R.id.ivClip);
         btnClip.setOnClickListener(new View.OnClickListener() {
-
 
             @Override
             public void onClick(View view) {
@@ -51,28 +44,6 @@ public class ScrollingActivity extends AppCompatActivity {
                 drawable.setLevel(mDrawableLevel);
             }
         });
-
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_scrolling, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
